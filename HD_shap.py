d5b72af4c1803f3c940c5f6f2f6ae19b2d37c777
ruinie <ruinie@umich.edu>
2023-01-24 23:58:42 -0500

    pre-commit

--- HD_shap.py
+++ HD_shap.py
@@ -1,20 +1,22 @@
 import os
-import pandas as pd
-import numpy as np
+from datetime import datetime
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import shap
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import MDS
 from sklearn.metrics import pairwise_distances
-from func import log, ShapExplanation, ModelPerformance
-import shap
-from datetime import datetime
-import plotly.graph_objects as go
-import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
-model_name="rf"
+from func import ModelPerformance, ShapExplanation, log
+
+model_name = "rf"
 usetrain = False
 random_seed = 42
 
@@ -24,12 +26,12 @@ df_HD = pd.read_csv("data/Heart Disease Dataset.csv")
 clf = RandomForestClassifier(random_state=random_seed)
 
 ## initiate logger
-path = "logs/"+datetime.now().strftime("%m_%d")+"/"
+path = "logs/" + datetime.now().strftime("%m_%d") + "/"
 isExist = os.path.exists(path)
 if not isExist:
-   os.makedirs(path)
+    os.makedirs(path)
 logger = log(path=path, file=f"{model_name}_shap.logs")
-logger.info("-"*15+"Start Session!"+"-"*15)
+logger.info("-" * 15 + "Start Session!" + "-" * 15)
 
 # model implementation
 # accuracy log
@@ -42,27 +44,46 @@ y_pred, y_pred_class = clf_eval.prediction(df_HD, Use_train=usetrain)
 add_data = [y_pred_class]
 
 # initiate explainer using shap
-if usetrain==True:
-   explain_X = clf_eval.split[0]
-   explain_X = pd.DataFrame(explain_X, columns=clf_eval.split[-1])
+if usetrain == True:
+    explain_X = clf_eval.split[0]
+    explain_X = pd.DataFrame(explain_X, columns=clf_eval.split[-1])
 else:
-   explain_X = df_HD[clf_eval.split[-1]]
+    explain_X = df_HD[clf_eval.split[-1]]
 ep_explanation = ShapExplanation(dataset=explain_X)
 ep_profile = ep_explanation.explain_for_all(model=clf)
 
 profile_shap = pd.DataFrame((ep_profile))
-profile_shap.columns=clf_eval.split[-1]
+profile_shap.columns = clf_eval.split[-1]
 
 ## PCA
 
 # profile_shap_pca = pca.fit_transform((cos_dis))
-profile_shap_pca = pd.DataFrame(ep_explanation.dim_reduct(e_profile=profile_shap), columns=["PC1","PC2"])
+profile_shap_pca = pd.DataFrame(
+    ep_explanation.dim_reduct(e_profile=profile_shap), columns=["PC1", "PC2"]
+)
 
 pca_name = "MDS"
 model_name = str(clf).split("(")[0]
 
-ep_explanation.fig_display_2d(model=model_name, pca=profile_shap_pca,e_profile=profile_shap, add_dataset = add_data, save=True, pca_name = pca_name) 
+ep_explanation.fig_display_2d(
+    model=model_name,
+    pca=profile_shap_pca,
+    e_profile=profile_shap,
+    add_dataset=add_data,
+    save=True,
+    pca_name=pca_name,
+)
 
 ## read preprocessed data
-X_pca = pd.DataFrame(ep_explanation.dim_reduct(e_profile=explain_X), columns=["PC1","PC2"])
-ep_explanation.fig_display_2d(model=model_name, pca=X_pca, e_profile=explain_X, add_dataset = add_data, save=True, pca_name = pca_name, file_name="HD_raw")
+X_pca = pd.DataFrame(
+    ep_explanation.dim_reduct(e_profile=explain_X), columns=["PC1", "PC2"]
+)
+ep_explanation.fig_display_2d(
+    model=model_name,
+    pca=X_pca,
+    e_profile=explain_X,
+    add_dataset=add_data,
+    save=True,
+    pca_name=pca_name,
+    file_name="HD_raw",
+)
--- func.py
+++ func.py
@@ -1,43 +1,47 @@
-import os
 import logging
-from tqdm import tqdm
+import os
+
 import numpy as np
 import pandas as pd
-from lime import lime_tabular
 import plotly.express as px
 import shap
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, pairwise_distances
+from lime import lime_tabular
 from sklearn.manifold import MDS
+from sklearn.metrics import accuracy_score, pairwise_distances
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+
 
 class ModelPerformance:
     def __init__(self, model) -> None:
-        self.model=model
-        self.train_status=0
+        self.model = model
+        self.train_status = 0
 
-    def train_test(self, dataset, test_size = .33, train_set=None, test_set=None, seed=42):
+    def train_test(
+        self, dataset, test_size=0.33, train_set=None, test_set=None, seed=42
+    ):
         feat_name = list(set(dataset.columns.values).difference({"target"}))
         X = dataset[feat_name]
         y = dataset["target"]
-        
-        if train_set==None or test_set==None:
+
+        if train_set == None or test_set == None:
             X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
-            self.split = [X_train, X_test, y_train, y_test, feat_name] 
+            self.split = [X_train, X_test, y_train, y_test, feat_name]
         else:
-            X_train=train_set[feat_name]
-            X_test=test_set[feat_name]
-            y_train=train_set["target"]
-            y_test=test_set["target"]
+            X_train = train_set[feat_name]
+            X_test = test_set[feat_name]
+            y_train = train_set["target"]
+            y_test = test_set["target"]
 
         self.model.fit(X_train, y_train)
         pred = self.model.predict(X_test)
         self.acc = accuracy_score(pred, y_test)
-        self.train_status=1
+        self.train_status = 1
         return self.acc
-    
+
     def prediction(self, predict_dataset=None, Use_train=True, Use_test=False):
-        if self.train_status==0:
+        if self.train_status == 0:
             raise InterruptedError("Need to train the model")
         elif Use_train and Use_test:
             raise ValueError("Can't use training set and testing set at the same time")
@@ -50,115 +54,165 @@ class ModelPerformance:
             y = self.split[3]
         else:
             if "target" in predict_dataset.columns.values:
-                feat_name = list(set(predict_dataset.columns.values).difference({"target"}))
+                feat_name = list(
+                    set(predict_dataset.columns.values).difference({"target"})
+                )
                 X = predict_dataset[feat_name]
                 y = predict_dataset["target"]
             else:
-                X=predict_dataset
+                X = predict_dataset
 
         pred = self.model.predict(X)
 
         if isinstance(y, pd.Series):
-            pred_class = self.get_pred_judge(pred,y.to_numpy())
-        else: 
-            pred_class=None
+            pred_class = self.get_pred_judge(pred, y.to_numpy())
+        else:
+            pred_class = None
 
         return pred, pred_class
 
     def get_pred_judge(self, y_pred, y):
         y_pred_class = []
         for i in range(len(y)):
-            if y_pred[i]==1 and y[i]==1:
+            if y_pred[i] == 1 and y[i] == 1:
                 y_pred_class.append("TP")
-            elif y_pred[i]==1 and y[i]==0:
+            elif y_pred[i] == 1 and y[i] == 0:
                 y_pred_class.append("FP")
-            elif y_pred[i]==0 and y[i]==1:
+            elif y_pred[i] == 0 and y[i] == 1:
                 y_pred_class.append("FN")
             else:
                 y_pred_class.append("TN")
         return y_pred_class
-    
+
 
 class EP:
     def __init__(self, dataset) -> None:
-        self.dataset=dataset
+        self.dataset = dataset
 
-    def dim_reduct(self, e_profile, method=MDS(), distance="cosine", seed=42, transform = StandardScaler()):
+    def dim_reduct(
+        self,
+        e_profile,
+        method=MDS(),
+        distance="cosine",
+        seed=42,
+        transform=StandardScaler(),
+    ):
         if transform is not None:
-            dis=transform.fit_transform(e_profile)
+            dis = transform.fit_transform(e_profile)
 
-        if distance=="cosine":
+        if distance == "cosine":
             pca = MDS(n_components=2, random_state=seed, dissimilarity="precomputed")
-            dis = 1-pairwise_distances(dis, metric="cosine")
+            dis = 1 - pairwise_distances(dis, metric="cosine")
         else:
             pca = MDS(n_components=2, random_state=seed)
-        
+
         return pca.fit_transform((dis))
 
-    def fig_display_2d(self, pca, pca_name, model, e_profile, add_dataset, file_name=None, save=True):
+    def fig_display_2d(
+        self, pca, pca_name, model, e_profile, add_dataset, file_name=None, save=True
+    ):
         print(self.dataset)
-        df_px_1 = pd.concat((pca, e_profile, pd.DataFrame(add_dataset, index = ["hover name"]).T,pd.DataFrame(np.repeat("att",len(self.dataset)),columns=["data type"])), axis=1)
-        df_px_2 = pd.concat((pca, self.dataset, pd.DataFrame(add_dataset, index = ["hover name"]).T, pd.DataFrame(np.repeat("raw",len(self.dataset)),columns=["data type"])), axis=1)
-        df_px = pd.concat((df_px_1, df_px_2),axis=0)
+        df_px_1 = pd.concat(
+            (
+                pca,
+                e_profile,
+                pd.DataFrame(add_dataset, index=["hover name"]).T,
+                pd.DataFrame(
+                    np.repeat("att", len(self.dataset)), columns=["data type"]
+                ),
+            ),
+            axis=1,
+        )
+        df_px_2 = pd.concat(
+            (
+                pca,
+                self.dataset,
+                pd.DataFrame(add_dataset, index=["hover name"]).T,
+                pd.DataFrame(
+                    np.repeat("raw", len(self.dataset)), columns=["data type"]
+                ),
+            ),
+            axis=1,
+        )
+        df_px = pd.concat((df_px_1, df_px_2), axis=0)
 
-        if file_name==None:
-            file_name=  f"HD_shap_{model}_{pca_name}_EP"
+        if file_name == None:
+            file_name = f"HD_shap_{model}_{pca_name}_EP"
 
-        fig = px.scatter(df_px.round(4), x="PC1", y="PC2" ,color="hover name", hover_name = "hover name", facet_col="data type",
-                        hover_data=self.dataset.columns.values, opacity=0.8, title=file_name,
-                    labels={"color": "hover name"})  
+        fig = px.scatter(
+            df_px.round(4),
+            x="PC1",
+            y="PC2",
+            color="hover name",
+            hover_name="hover name",
+            facet_col="data type",
+            hover_data=self.dataset.columns.values,
+            opacity=0.8,
+            title=file_name,
+            labels={"color": "hover name"},
+        )
 
-        if save==False:             
+        if save == False:
             fig.show()
         else:
             fig.write_html(f"html/{file_name}.html")
 
+
 class LimeExplaination(EP):
     def __init__(self, seed=42) -> None:
         super(EP, self).__init__()
-        self.seed=seed
+        self.seed = seed
 
-    def explain_for_all(self, model, mode = "classification"):
-        """
-
-        """
-        explainer = lime_tabular.LimeTabularExplainer(self.X, feature_names=self.feat_name, verbose=False, mode=mode, random_state=self.seed)
+    def explain_for_all(self, model, mode="classification"):
+        """ """
+        explainer = lime_tabular.LimeTabularExplainer(
+            self.X,
+            feature_names=self.feat_name,
+            verbose=False,
+            mode=mode,
+            random_state=self.seed,
+        )
         profile_lime = dict()
         for j in tqdm(np.arange(len(self.X))):
             if mode == "classification":
-                exp = explainer.explain_instance(self.X[j,:], model.predict_proba, num_features = len(self.feat_name))
+                exp = explainer.explain_instance(
+                    self.X[j, :], model.predict_proba, num_features=len(self.feat_name)
+                )
             else:
-                exp = explainer.explain_instance(self.X[j,:], model.predict, num_features = len(self.feat_name))
-            exp_prof_lime=dict()
+                exp = explainer.explain_instance(
+                    self.X[j, :], model.predict, num_features=len(self.feat_name)
+                )
+            exp_prof_lime = dict()
             exp_value = exp.as_map()[1]
             for i in range(len(exp_value)):
                 ind_col = exp_value[i][0]
                 exp_prof_lime[self.feat_name[ind_col]] = exp_value[i][1]
             profile_lime[j] = exp_prof_lime
 
-        return(pd.DataFrame(profile_lime).T)
+        return pd.DataFrame(profile_lime).T
 
 
 class ShapExplanation(EP):
     def __init__(self, dataset, seed=42) -> None:
         super().__init__(dataset)
-        self.seed=seed
+        self.seed = seed
 
     def explain_for_all(self, model):
-        explainer = shap.Explainer(model,seed=42)
+        explainer = shap.Explainer(model, seed=42)
         shap_values = explainer(self.dataset)
-        profile_shap = (shap_values.values[...,0])
+        profile_shap = shap_values.values[..., 0]
         return pd.DataFrame(profile_shap)
 
-    # def 
+    # def
+
 
 def log(path, file):
     """[Create a log file to record the experiment's logs]
-    
+
     Arguments:
         path {string} -- path to the directory
         file {string} -- file name
-    
+
     Returns:
         [obj] -- [logger that record logs]
     """
@@ -175,13 +229,13 @@ def log(path, file):
     # configure logger
     logging.basicConfig(level=logging.INFO, format=console_logging_format)
     logger = logging.getLogger()
-    
+
     # create a file handler for output file
     handler = logging.FileHandler(log_file)
 
     # set the logging level for log file
     handler.setLevel(logging.INFO)
-    
+
     # create a logging format
     formatter = logging.Formatter(file_logging_format)
     handler.setFormatter(formatter)
